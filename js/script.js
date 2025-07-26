1773cac243c9b04e97e5f19d16a7b8e12be2cbb4
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-27 02:21:20 +0800

    Save working state before further changes

--- js/script.js
+++ js/script.js
@@ -4,24 +4,6 @@ display.textContent = '0';
 
 
 btn_con.addEventListener('click', displayValues);
-	
-	// displayValues;
-
-	// let val1 = 0;
-	// let val2 = 0;
-
-	// if(isNaN(id)) {
-	// 	switch(id) {
-	// 		case 'Add':
-	// 			console.log('add');
-	// 			display.textContent += '+';
-	// 			val1 = parseInt(display.textContent);
-	// 			break;
-	// 		case 'AC':
-	// 			display.textContent = '0';
-	// 	}
-	// }
-// });
 
 let checkOperator = false;
 let operatorClicked;
@@ -31,36 +13,51 @@ function displayValues(e) {
 	let splitId = e.target.id.split('btn');
 	let id = splitId[1];
 	
-
 	if(!isNaN(id)) {
+		// Removes operator highlight when digit is pressed
 		operatorClicked ? operatorClicked.classList.remove('lit-up') : false;
-
+		// Reset display
 		if(checkOperator == true) { display.textContent = 0; checkOperator = false; }
 		if(display.textContent == '0') { display.textContent = ''; }
 		display.textContent += id;
 	} else {
+		// Removes operator highlight when another operator is pressed
+		operatorClicked ? operatorClicked.classList.remove('lit-up') : false;
 		if(!operatorClicked) {operatorClicked = document.querySelector(`#` + e.target.id);}
 		operatorClicked = document.querySelector(`#` + e.target.id);
 		operatorClicked.classList.add('lit-up');
-		console.log(operatorClicked);
+		// console.log(operatorClicked);
 		calculate(id, parseInt(display.textContent));
+		
 	}
 }
 
+let result = 0;
+let result2;
 let val1;
 let val2;
 
 function calculate(operator, displayValue) {
-	if(!val1) {val1 = displayValue; }
-	else { val2 = displayValue; }
-	
-	console.log("Display value:" + displayValue);
+	if(!val1) {val1 = displayValue; val2 = 0; } else { val2 = displayValue;  }
+	// console.log("RESULT: " + result);
+	// console.log("VAL1: " + val1);
+	// console.log("VAL2: " + val2);
+	// console.log("--------------------------");
 	switch(operator) {
 		case 'Add': 
-			checkOperator = true;
-			if(!val2) {val2 = 0;}
-			val1 += val2;
-			display.textContent = val1;
+			result = val1+val2;
+			val1 = result;
+			break;
+		case 'Subtract':
+			result = val1-val2;
+			val1 = result;
 			break;
 	}
+	// console.log("RESULT: " + result);
+	console.log("VAL1: " + val1);
+	console.log("VAL2: " + val2);
+	console.log("========================");
+
+	display.textContent = result;
+	checkOperator = true;
 }
