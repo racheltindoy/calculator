62d4ad4f95da9b6cd8f749218f47179f625d5549
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-26 23:30:21 +0800

    Implement number reset after operator is pressed

--- js/script.js
+++ js/script.js
@@ -3,29 +3,50 @@ const display = document.querySelector('#display');
 display.textContent = '0';
 
 
-btn_con.addEventListener('click', (e) => {
+btn_con.addEventListener('click', displayValues);
+	
+	// displayValues;
+
+	// let val1 = 0;
+	// let val2 = 0;
+
+	// if(isNaN(id)) {
+	// 	switch(id) {
+	// 		case 'Add':
+	// 			console.log('add');
+	// 			display.textContent += '+';
+	// 			val1 = parseInt(display.textContent);
+	// 			break;
+	// 		case 'AC':
+	// 			display.textContent = '0';
+	// 	}
+	// }
+// });
+
+let checkOperator = false;
+function displayValues(e) {
 	const isClickedButton = e.target.closest('button');
 	if(!isClickedButton) { return }
 	let splitId = e.target.id.split('btn');
 	let id = splitId[1];
-
 	if(!isNaN(id)) {
+		if(checkOperator == true) { display.textContent = ''; checkOperator = false; }
 		if(display.textContent == '0') { display.textContent = ''; }
 		display.textContent += id;
+	} else {
+		calculate(id, parseInt(display.textContent));
 	}
+}
 
-	let val1 = 0;
-	let val2 = 0;
+let val1;
+let val2;
 
-	if(isNaN(id)) {
-		switch(id) {
-			case 'Add':
-				console.log('add');
-				display.textContent += '+';
-				val1 = display.textContent;
-				break;
-			case 'AC':
-				display.textContent = '0';
-		}
+function calculate(operator, displayValue) {
+	if(!val1) {val1 = displayValue; }
+	console.log(displayValue);
+	switch(operator) {
+		case 'Add': 
+			checkOperator = true;
+			break;
 	}
-});
+}
