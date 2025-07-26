574668626688f6b15632fdcb169fdda8c2107d3b
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-26 23:55:12 +0800

    Implement addition functionality

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ function displayValues(e) {
 	let splitId = e.target.id.split('btn');
 	let id = splitId[1];
 	if(!isNaN(id)) {
-		if(checkOperator == true) { display.textContent = ''; checkOperator = false; }
+		if(checkOperator == true) { display.textContent = 0; checkOperator = false; }
 		if(display.textContent == '0') { display.textContent = ''; }
 		display.textContent += id;
 	} else {
@@ -43,10 +43,15 @@ let val2;
 
 function calculate(operator, displayValue) {
 	if(!val1) {val1 = displayValue; }
-	console.log(displayValue);
+	else { val2 = displayValue; }
+	
+	console.log("Display value:" + displayValue);
 	switch(operator) {
 		case 'Add': 
 			checkOperator = true;
+			if(!val2) {val2 = 0;}
+			val1 += val2;
+			display.textContent = val1;
 			break;
 	}
 }
