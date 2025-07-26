1ea8dd984bd991009bc6b249ce975ceb535d7f88
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-27 02:43:32 +0800

    Refactored light on button effects

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ display.textContent = '0';
 
 btn_con.addEventListener('click', displayValues);
 
-let checkOperator = false;
+let isOperatorClicked = false;
 let operatorClicked;
 function displayValues(e) {
 	const isClickedButton = e.target.closest('button');
@@ -13,20 +13,17 @@ function displayValues(e) {
 	let splitId = e.target.id.split('btn');
 	let id = splitId[1];
 	
+	// Removes operator highlight when digit is pressed
+	lightOffButton();
+
 	if(!isNaN(id)) {
-		// Removes operator highlight when digit is pressed
-		operatorClicked ? operatorClicked.classList.remove('lit-up') : false;
 		// Reset display
-		if(checkOperator == true) { display.textContent = 0; checkOperator = false; }
+		if(isOperatorClicked == true) { display.textContent = 0; isOperatorClicked = false; }
 		if(display.textContent == '0') { display.textContent = ''; }
 		display.textContent += id;
 	} else {
 		// Removes operator highlight when another operator is pressed
-		operatorClicked ? operatorClicked.classList.remove('lit-up') : false;
-		if(!operatorClicked) {operatorClicked = document.querySelector(`#` + e.target.id);}
-		operatorClicked = document.querySelector(`#` + e.target.id);
-		operatorClicked.classList.add('lit-up');
-		// console.log(operatorClicked);
+		lightUpButton(e);
 		calculate(id, parseInt(display.textContent));
 		
 	}
@@ -59,5 +56,18 @@ function calculate(operator, displayValue) {
 	console.log("========================");
 
 	display.textContent = result;
-	checkOperator = true;
+	isOperatorClicked = true;
+}
+
+
+
+function lightUpButton(e) {
+	
+	if(!operatorClicked) {operatorClicked = document.querySelector(`#` + e.target.id);}
+	operatorClicked = document.querySelector(`#` + e.target.id);
+	operatorClicked.classList.add('lit-up');
+}
+
+function lightOffButton() {
+	operatorClicked ? operatorClicked.classList.remove('lit-up') : false;
 }
