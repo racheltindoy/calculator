dc82b7387c8027ce38df831ac465fcf17722c698
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-27 17:32:32 +0800

    Refactor Del logic into removeLastDigit function

--- js/script.js
+++ js/script.js
@@ -29,16 +29,10 @@ function displayValues(e) {
 		console.log('VAL1: ' + val1);
 		console.log('VAL2: ' + val2);
 		console.log("--------------------------")
-		
-	} else if(id === 'AC') {
-		resetValues();
-	} else if (id === 'Del') {
-		let displayLength = display.textContent.length
-		// console.log(display.textContent.slice(0, -1));
-		let newDisplay = display.textContent = display.textContent.slice(0, -1);
-		val1 = parseInt(newDisplay);
-		console.log("VAL1 NEW DISPLAY: " + val1);
-	} else {
+	}
+	else if(id === 'AC') { resetValues(); } 
+	else if (id === 'Del') { removeLastDigit(); } 
+	else {
 		// Removes operator highlight when another operator is pressed
 		if(isOperatorClicked == true) {
 			lightUpButton(e); 
@@ -102,6 +96,11 @@ function calculate() {
 	}
 
 	isOperatorClicked = true;
+}
+
+function removeLastDigit() {
+	let newDisplay = display.textContent = display.textContent.slice(0, -1);
+	val1 = parseInt(newDisplay);
 }
 
 function resetValues() {
